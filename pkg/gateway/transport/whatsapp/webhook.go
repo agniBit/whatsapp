dec3689037844616bf8de7d3a0b50aa34ffc29e9
Abhishek Agnihotri <abhishek@theflurn.com>
2022-07-22 13:15:41 +0530

    fix: yaml tagname

--- pkg/gateway/transport/whatsapp/webhook.go
+++ pkg/gateway/transport/whatsapp/webhook.go
@@ -1,7 +1,6 @@
 package transport
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -11,10 +10,13 @@ func (h *HTTP) verifyCallback(c echo.Context) error {
 	challenge := c.QueryParam("hub.challenge")
 	mode := c.QueryParam("subscribe")
 	verifyToken := c.QueryParam("hub.verify_token")
-	fmt.Println(challenge)
+
+	// check if the verify token is correct and the mode is subscribe
 	if mode == "subscribe" && h.cfg.Server.SubscribeVerifyToken == verifyToken {
+		// return the challenge
 		return c.String(http.StatusOK, challenge)
 	}
+
 	return c.JSON(http.StatusUnauthorized, http.StatusText(http.StatusUnauthorized))
 }
 
