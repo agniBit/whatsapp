6b99a10b3c4dac9c70f37a5aa3c0289fb40849ef
Abhishek Agnihotri <agni.abhi00@gmail.com>
2022-07-24 02:28:53 +0530

    refactor: move send message function in whatsapp.go

--- pkg/whatsapp/whatsapp.go
+++ pkg/whatsapp/whatsapp.go
@@ -1,6 +1,15 @@
 package whatsapp
 
-import "github.com/agniBit/whatsapp/pkg/gateway"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/agniBit/whatsapp/pkg/gateway"
+	"github.com/agniBit/whatsapp/type/whatsapp"
+)
 
 type HTTP struct {
 	svc *gateway.Service
@@ -10,4 +19,49 @@ func NewHTTP(svc *gateway.Service) *HTTP {
 	return &HTTP{
 		svc: svc,
 	}
+}
+
+func (waS waService) SendMessage(payload whatsapp.WaMessagePayload) (*whatsapp.WaResponse, error) {
+	method := "POST"
+
+	payloadString, err := json.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+
+	payloadReader := strings.NewReader(string(payloadString))
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, waS.cgf.Whatsapp.URL, payloadReader)
+
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+waS.cgf.Whatsapp.Token)
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	fmt.Println(string(body))
+
+	resBody := &whatsapp.WaResponse{}
+	err = json.Unmarshal(body, &resBody)
+
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	return resBody, nil
 }
